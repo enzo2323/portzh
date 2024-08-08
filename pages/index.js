f141de36b3ba540924d867c97632f7c62cbb3f8e
enzo2323 <58128131+enzo2323@users.noreply.github.com>
2024-08-08 16:25:43 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my Portfolio" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          I am a UI designer specialized in intuitive and attractive interfaces. I transform ideas into unique digital experiences as a website creator. 🎥 Video editor, providing creativity and precision to bring your stories to life. 🤖 Expert in creating and configuring Discord bots, optimizing interaction and server management. ✏️ Passionate draftsman, capturing creativity in every stroke. My focus is to provide innovative and effective solutions that exceed my clients expectations, to unique design, web development, video editing, Discord bots and art. <code>pages/index.js</code>
         </p>
       </main>
 
